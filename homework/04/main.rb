6aade742c00d8a9439897ee5493ecc21c07176b8
Honza Bittner <git@tenhobi.dev>
2021-10-21 21:42:50 +0200

    Implement rest of the methods

--- homework/04/main.rb
+++ homework/04/main.rb
@@ -1,19 +1,17 @@
+# frozen_string_literal: true
+
 require_relative 'my_struct'
 
-struct = MyStruct.new
-
-begin
+def test
+  struct = MyStruct.new
+  struct.name = 'John'
   puts struct.name
-rescue
-  puts "no [name] field #1"
+
+  struct[:age] = 42
+  puts struct[:age]
+
+  struct.delete_field :age
+  struct.each_pair.to_a
+
+  puts struct.each_pair { |key, value| [key.to_s.upcase, value.to_s.upcase] }.to_h
 end
-
-struct.name = "John"
-puts struct.name
-struct[:age] = 42
-puts struct[:age]
-
-
-struct.delete_field :age
-
-p struct.each_pair.to_a
